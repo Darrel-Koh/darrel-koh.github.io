4052b48e08e4890311c2cc1901ab23462de9f312
Darrel-Koh <koh465@gmail.com>
2023-05-15 20:51:58 +0800

    added search table functionality to fit 1 column

--- SystemAdmin/JSfiles/profile-management.js
+++ SystemAdmin/JSfiles/profile-management.js
@@ -20,46 +20,42 @@ function myFunction() {
     }
   }
 
-  // Example data of user objects
-      var users = [
-  { id: 1, name: "Alfreds Futterkiste", role: "System Admin", email: "alfred@example.com" },
-  { id: 2, name: "Berglunds snabbkop", role: "Manager", email: "berglunds@example.com" },
-  { id: 3, name: "Jack neo", role: "Staff", email: "jack@example.com" },
-  { id: 4, name: "James Hemsworth", role: "Customer", email: "handsome@example.com" },
-  // Add more user objects as needed
+// Example data of user objects
+var users = [
+    { id: 1, role: "System Admin" },
+    { id: 2, role: "Manager" },
+    { id: 3, role: "Staff" },
+    { id: 4, role: "Customer" },
+    // Add more user objects as needed
   ];
-
+  
   // Function to generate the table rows dynamically
   function generateTable() {
-  var tableBody = document.getElementById("tableBody");
-  tableBody.innerHTML = ""; // Clear previous table rows
-
-  users.forEach(function (user) {
+    var tableBody = document.getElementById("tableBody");
+    tableBody.innerHTML = ""; // Clear previous table rows
+  
+    users.forEach(function (user) {
       var row = document.createElement("tr");
-      var nameCell = document.createElement("td");
-      var nameLink = document.createElement("a");
-      nameLink.href = "./view-user-details.html?userId=" + user.id; // Modify the href value with the appropriate link destination
-
-      nameLink.textContent = user.name; // Set the text content of the link
-
-      // Attach event listener to handle click event
-      nameLink.addEventListener("click", function (event) {
-      event.preventDefault(); // Prevent the default hyperlink behavior
-      var userId = user.id; // Get the user ID from the user object
-      // Call a function or perform any desired action with the user ID
-      displayUserDetails(userId);
-      });
-
-      nameCell.appendChild(nameLink);
-      row.appendChild(nameCell);
-      row.innerHTML += `
-      <td>${user.role}</td>
-      <td>${user.email}</td>
-      `;
+  
+      // Create a hidden input field for user ID
+      var userIdInput = document.createElement("input");
+      userIdInput.type = "hidden";
+      userIdInput.value = user.id;
+  
+      // Create the role cell with hyperlink
+      var roleCell = document.createElement("td");
+      var roleLink = document.createElement("a");
+      roleLink.href = "./view-user-details.html?userId=" + user.id; // Modify the href value with the appropriate link destination
+      roleLink.textContent = user.role; // Set the text content of the link
+  
+      roleCell.appendChild(roleLink);
+      row.appendChild(userIdInput);
+      row.appendChild(roleCell);
       tableBody.appendChild(row);
-  });
+    });
   }
-generateTable(); // Call the function to generate the table initially
+  
+  generateTable(); // Call the function to generate the table initially
 
 
 let menu = document.querySelector('#menu-bars');
