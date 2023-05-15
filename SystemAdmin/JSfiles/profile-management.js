73c11987a363b3973d333e2c17d8ac9157ebaaee
Darrel-Koh <koh465@gmail.com>
2023-05-15 23:38:05 +0800

    removed extra search button

--- SystemAdmin/JSfiles/profile-management.js
+++ SystemAdmin/JSfiles/profile-management.js
@@ -69,12 +69,6 @@ window.onscroll = () =>{
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
 }
-document.querySelector('#search-icon').onclick = () =>{
-    document.querySelector('#search-form').classList.toggle('active');
-}
-document.querySelector('#close').onclick = () =>{
-    document.querySelector('#search-form').classList.remove('active');
-}
 // Initialize Swiper 
 var swiper = new Swiper(".home-slider", {
   spaceBetween: 30,
